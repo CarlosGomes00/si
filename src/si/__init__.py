2f0de0e61aec3075d3d850dc4d81ea1838042e25
CarlosGomes00 <carlosfcgomes0@gmail.com>
2024-12-06 23:49:34 +0000

    minor fix

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,9 +1,9 @@
-__author__ = "João Capela"
-__credits__ = ["João Capela", "João Correia, Fernando Cruz", "Vítor Pereira"]
+__author__ = "João Capela, Carlos Gomes"
+__credits__ = ["João Capela", "João Correia, Fernando Cruz", "Vítor Pereira", "Carlos Gomes"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
-__maintainer__ = "João Capela"
-__email__ = "joao.capela@ceb.uminho.pt"
+__maintainer__ = "João Capela, Carlos Gomes"
+__email__ = "joao.capela@ceb.uminho.pt, carlosfcgomes0@gmail.com"
 
 # -----------------------------------------------------------------------------------
 # This package is heavily inspired and adapted from
