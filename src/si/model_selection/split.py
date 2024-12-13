708e6a1d96936fb8e156cefc611ad400c05a01dd
CarlosGomes00 <carlosfcgomes0@gmail.com>
2024-12-13 12:48:00 +0000

    stratified_train_tes_split added

--- src/si/model_selection/split.py
+++ src/si/model_selection/split.py
@@ -41,3 +41,48 @@ def train_test_split(dataset: Dataset, test_size: float = 0.2, random_state: int
     train = Dataset(dataset.X[train_idxs], dataset.y[train_idxs], features=dataset.features, label=dataset.label)
     test = Dataset(dataset.X[test_idxs], dataset.y[test_idxs], features=dataset.features, label=dataset.label)
     return train, test
+
+def stratified_train_test_split(dataset : Dataset, test_size : float = 0.2, random_state : int = None) -> Tuple[Dataset, Dataset]:
+
+    """
+    Perform a stratified train-test split on a dataset
+
+    Parameters:
+    ----------
+        dataset: Dataset
+            An object with `features` and `labels` attributes
+        test_size: float
+            Proportion of the dataset to include in the test split (default: 0.2)
+        random_state: int
+            Seed for reproducibility (default: None)
+
+    Returns
+    ----------
+        train_set, test_set: tuple[Dataset, Dataset]
+            Two Dataset objects, one for training and one for testing
+    """
+
+    unique_labels = np.unique(dataset.y)
+
+    train_indices = []
+    test_indices = []
+
+    if random_state is not None:
+        np.random.seed(random_state)
+
+    for label in unique_labels:
+        label_indices = np.where(dataset.y == label)[0]
+
+        np.random.shuffle(label_indices)
+
+        n_label_test_samples = max(1, int(len(label_indices) * test_size))
+
+        test_indices.extend(label_indices[:n_label_test_samples])
+        train_indices.extend(label_indices[n_label_test_samples:])
+
+    train_dataset = Dataset(X=dataset.X[train_indices], y=dataset.y[train_indices])
+    test_dataset = Dataset(X=dataset.X[test_indices], y=dataset.y[test_indices])
+
+    return train_dataset, test_dataset
+
+#TODO FAZER TESTES
