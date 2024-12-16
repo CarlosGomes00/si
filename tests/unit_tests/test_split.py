e94f9c7915a95df76ce4f7e68044fef075840a4b
CarlosGomes00 <carlosfcgomes0@gmail.com>
2024-12-16 22:47:30 +0000

    Testsplits finished

--- tests/unit_tests/test_split.py
+++ tests/unit_tests/test_split.py
@@ -24,14 +24,14 @@ class TestSplits(TestCase):
 
     def test_stratified_split_output_type(self):
 
-        train, test = stratified_train_test_split(self.dataset, test_size=0.2, random_state=42)
+        train, test = stratified_train_test_split(self.dataset, test_size=0.2, random_state=123)
         self.assertIsInstance(train, type(self.dataset), "Train dataset isn't a Dataset type")
         self.assertIsInstance(test, type(self.dataset), "Test dataset isn't a Dataset type")
 
     def test_stratified_split_proportion(self):
 
         test_size = 0.2
-        train, test = stratified_train_test_split(self.dataset, test_size=test_size, random_state=42)
+        train, test = stratified_train_test_split(self.dataset, test_size=test_size, random_state=123)
 
         total_samples = len(self.dataset.y)
         expected_test_size = int(total_samples * test_size)
@@ -42,11 +42,28 @@ class TestSplits(TestCase):
 
     def test_stratified_split_random_state(self):
 
-        train1, test1 = stratified_train_test_split(self.dataset, test_size=0.2, random_state=42)
-        train2, test2 = stratified_train_test_split(self.dataset, test_size=0.2, random_state=42)
+        train1, test1 = stratified_train_test_split(self.dataset, test_size=0.2, random_state=123)
+        train2, test2 = stratified_train_test_split(self.dataset, test_size=0.2, random_state=123)
 
         np.testing.assert_array_equal(train1.X, train2.X, "Training data isn't consistent")
         np.testing.assert_array_equal(test1.X, test2.X, "Test data isn't consistent")
 
+    def test_stratified_split_stratification(self):
 
-        #TODO Adicionar mais um para verificar se continua balanceado
+        train, test = stratified_train_test_split(self.dataset, test_size=0.2, random_state=123)
+
+        unique_labels, original_counts = np.unique(self.dataset.y, return_counts=True)
+        original_ratios = original_counts / len(self.dataset.y)
+
+        _, train_counts = np.unique(train.y, return_counts=True)
+        _, test_counts = np.unique(test.y, return_counts=True)
+
+        train_ratios = train_counts / len(train.y)
+        test_ratios = test_counts / len(test.y)
+
+        np.testing.assert_allclose(original_ratios, train_ratios, atol=0.01,
+                                   err_msg="The proportions in the training set don't match the originals")
+        np.testing.assert_allclose(original_ratios, test_ratios, atol=0.01,
+                                   err_msg="The proportions in the test set don't match the originals")
+
+
